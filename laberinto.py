f846157ee16d931f20df09fcb49504a0a76224f7
paulaanb <paulanb2000@gmail.com>
2021-12-09 10:09:31 +0100

    Establecemos las coordenadas de nuestro laberinto

--- laberinto.py
+++ laberinto.py
@@ -5,3 +5,13 @@ import random
 import re
 import sys
 
+#Establecemos las coordenadas de nuestro laberinto
+class Coordenadas:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    def comparate(self,x,y):
+        if(self.x==x and self.y==y):
+            return True
+        else:
+            return False
