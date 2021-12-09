cfdf89e953ce6215aeaca74dbd56d0572289a883
paulaanb <paulanb2000@gmail.com>
2021-12-09 10:36:07 +0100

    redefinimos las coordenadas para un mayor entendimiento

--- laberinto.py
+++ laberinto.py
@@ -10,16 +10,21 @@ class Coordenadas:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+        place_x = 0
+        place_y = 0
     def comparate(self,x,y):
         if(self.x==x and self.y==y):
             return True
         else:
             return False
+        
 #Definimos el tunel
 class Tunel:
     def __init__(self, x1, y1, x2, y2):
         self.extremo1 = Coordenadas(x1, y1)
         self.extremo2 = Coordenadas(x2, y2)
+    global place_x
+    global place_y
         
 #El laberinto resuelto es el siguiente:
 lab_solution =[
@@ -33,3 +38,12 @@ lab_solution =[
 #Como salir de el
 howtogetout = ["ABAJO", "ABAJO", "ABAJO", "ABAJO","DERECHA", "ABAJO", "DERECHA", "DERECHA", "ARRIBA", "ARRIBA", "IZQUIERDA", "ARRIBA", "ARRIBA", "DERECHA", "DERECHA", "ABAJO"]
 
+#Definimos la función para poder movernos 
+def movement():
+    print("1-> DERECHA")
+    print("2-> IZQUIERDA")
+    print("3-> ARRIBA")
+    print("4-> ABAJO")
+    movement = int(input("Escoja un movimiento entre los disponibles."))
+    if movement == 1:
+        
