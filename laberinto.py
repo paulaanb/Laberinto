002e75a8b64b957f53e19acdfd48a3b1444556a4
paulaanb <paulanb2000@gmail.com>
2021-12-09 11:05:42 +0100

    Definimos la función para poder movernos
    Hemos corregido los fallos anteriores y ampliado el laberinto para una dificultad más elevada

--- laberinto.py
+++ laberinto.py
@@ -10,33 +10,31 @@ class Coordenadas:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-    place_x = 0
-    place_y = 0
+        
     def comparate(self,x,y):
         if(self.x==x and self.y==y):
             return True
         else:
             return False
-        
+place_x = 0
+place_y = 0
 #Definimos el tunel
 class Tunel:
     def __init__(self, x1, y1, x2, y2):
         self.extremo1 = Coordenadas(x1, y1)
         self.extremo2 = Coordenadas(x2, y2)
-    global place_x
-    global place_y
         
 #El laberinto resuelto es el siguiente:
 lab_solution =[
-    " ", "X", "X", "X", "X",
-    " ", "X", " ", " ", " ",
-    " ", "X", " ", "X", "S",
-    " ", "X", " ", " ", "X",
-    " ", " ", "X", " ", "X",
-    "X", " ", " ", " ", "X",
+    " ", "X", "X", "X", "X", "X",
+    " ", "X", " ", " ", " ", " ",
+    " ", "X", " ", "X", "X", "S",
+    " ", "X", " ", " ", "X", "X",
+    " ", " ", "X", " ", "X", "X",
+    "X", " ", " ", " ", "X", "X",
 ]
 #Como salir de el
-howtogetout = ["ABAJO", "ABAJO", "ABAJO", "ABAJO","DERECHA", "ABAJO", "DERECHA", "DERECHA", "ARRIBA", "ARRIBA", "IZQUIERDA", "ARRIBA", "ARRIBA", "DERECHA", "DERECHA", "ABAJO"]
+howtogetout = ["ABAJO", "ABAJO", "ABAJO", "ABAJO","DERECHA", "ABAJO", "DERECHA", "DERECHA", "ARRIBA", "ARRIBA", "IZQUIERDA", "ARRIBA", "ARRIBA", "DERECHA", "DERECHA", "DERECHA", "ABAJO"]
 
 #Definimos la función para poder movernos 
 def movement():
@@ -46,4 +44,21 @@ def movement():
     print("4-> ABAJO")
     movement = int(input("Escoja un movimiento entre los disponibles."))
     if movement == 1:
-        place_x 
+        place_x += 1
+        return place_x
+    if movement == 2:
+        place_x -= 1
+        if place_x > 0 and place_x < 6:
+            return place_x
+        else: 
+            print("Ese movimiento ha generado la salida del laberinto.")
+    if movement == 3:
+        place_y -= 1 
+        if place_y > 0 and place_y < 6:
+            return place_y
+        else:
+           print("Ese movimiento ha generado la salida del laberinto.") 
+    if movement == 4:
+        place_y += 1
+        return place_y
+
